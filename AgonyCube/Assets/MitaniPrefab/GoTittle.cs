def51eae5f0b34de88359a74b86f0373e5d19af9
CP5S37 <kaito.mitani.0223@gmail.com>
2018-10-16 19:25:03 +0900

    ooo

---
+++ AgonyCube/Assets/MitaniPrefab/GoTittle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GoTittle : MonoBehaviour
+{
+
+    //　スタートボタンを押したら実行する
+    public void SceneLoad()
+    {
+        SceneManager.LoadScene("Tittle_mitani");
+    }
+}
