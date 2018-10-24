ec87f372050f6c08bbeec3307f6b3b7db78c3150
hoshi <fyhcs0605@gmail.com>
2018-10-24 19:35:39 +0900

    mainstage update

---
+++ AgonyCube/Assets/Scripts/MainStage/CharaterController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AgonyCubeMainStage {
+    public class CharaterController : MonoBehaviour {
+
+        CharacterController Controller;
+
+        public float Speed;
+        public float Gravity;
+        Vector3 MoveDirection;
+        private Animator animator;
+        public Transform target;
+
+
+        // Use this for initialization
+        void Start() {
+            Controller = GetComponent<CharacterController>();
+            animator = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+
+            if (Controller.isGrounded) {
+                MoveDirection = new Vector3(0, 0, 1);
+                MoveDirection = transform.TransformDirection(MoveDirection);
+                MoveDirection *= Speed;
+            }
+            //MoveDirection.y -= Gravity * Time.deltaTime;
+            transform.LookAt(target);
+
+            // Controller.Move(MoveDirection * Time.deltaTime);
+        }
+
+    }
+}
---
+++ AgonyCube/Assets/Scripts/MainStage/StepController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AgonyCubeMainStage {
+    public class StepController : MonoBehaviour {
+
+        public GameObject Wall;
+        public GameObject Floor;
+        public GameObject[] HitCheckCollider;
+
+        public LayerMask PlayerLayer;
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+
+
+        }
+
+        public bool StepMoveCheck() {
+
+            if (Floor.transform.position.y <= (transform.position.y + 0.5)) {
+                if (Wall.GetComponent<Wall>().FloorCheck == false) {
+                    Ray stepray = new Ray(transform.position, -transform.forward);
+                    RaycastHit hit;
+
+
+                    if (Physics.Raycast(stepray, out hit, 3, PlayerLayer)) {
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }
+                }
+                else {
+                    return false;
+                }
+
+            }
+            else {
+                return false;
+            }
+        }
+
+
+
+    }
+}
