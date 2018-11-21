d0ea188121eeaed3bf5d645258bf328bff268872
CP5S34 <yokohama.vcp@gmail.com>
2018-11-21 17:06:39 +0900

    stage3を編集

---
+++ AgonyCube/Assets/Scripts/Stage3/Vol.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Vol : MonoBehaviour {
+
+
+    public UnityEngine.Audio.AudioMixer mixer;
+
+    public void BGMVol(Slider slider)
+    {
+        mixer.SetFloat("BGM", slider.value);
+    }
+}
