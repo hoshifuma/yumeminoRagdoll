4d36977a74aab734b8257a3d2dc9881691448819
CP5S33 <yokohama.vcp@gmail.com>
2018-11-21 16:59:20 +0900

    TestResult.unityの名前を変更 TestResult.unity関係etc

---
+++ AgonyCube/Assets/Scripts/Result/SwapPlayText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwapPlayText : MonoBehaviour {
+
+    private Text targetText2;
+
+    void swap()
+    {
+        this.targetText2 = this.GetComponent<Text>();
+        string resultSwapPlay = StageState.GetSwapPlay();
+        this.targetText2.text = resultSwapPlay;
+    }
+    // Use this for initialization
+    void Start () {
+        swap();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
