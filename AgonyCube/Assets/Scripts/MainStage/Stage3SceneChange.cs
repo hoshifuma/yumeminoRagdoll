db45492540085ee91d234ac63cc36a3c2ba02e3a
CP5S35 <yokohama.vcp@gmail.com>
2018-11-21 19:26:33 +0900

    いろいろしました

---
+++ AgonyCube/Assets/Scripts/MainStage/Stage3SceneChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stage3SceneChange : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void OnClickButton ()
+    {
+        SceneManager.LoadScene("Stage3");
+    }
+		
+}
