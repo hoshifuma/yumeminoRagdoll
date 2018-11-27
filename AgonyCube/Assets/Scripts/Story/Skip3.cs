9fb866817eea5cbd644e84297a7d306515e82b56
CP5S36 <yokohama.vcp@gmail.com>
2018-11-27 11:16:59 +0900

    それぞれのSceneへスキップ移動できるようにしました。

---
+++ AgonyCube/Assets/Scripts/Story/Skip3.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Skip3 : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	public void SkipButton()
+    {
+        SceneManager.LoadScene("MainScene");
+	}
+ }
---
+++ AgonyCube/Assets/Scripts/Story/Skip5.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Skip5 : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	public void SkipButton()
+    {
+        SceneManager.LoadScene("Story2-2");
+	}
+ }
