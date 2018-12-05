13c8979a648ac84759b5c34bb77f771697ff5cb4
CP5S34 <yokohama.vcp@gmail.com>
2018-12-05 19:27:46 +0900

    Add Loading Effect

---
+++ AgonyCube/Assets/Scripts/Title/SceneController2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace AgonyCube.Title
+{
+    // 『タイトル画面』のシーン遷移を管理します。
+    public class SceneController2 : MonoBehaviour
+    {
+        private void Start()
+        {
+            SceneManager.LoadScene("StageSelector");
+        }
+
+    }
+}
