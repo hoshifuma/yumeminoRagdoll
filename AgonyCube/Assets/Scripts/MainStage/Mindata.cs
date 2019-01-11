584a3f62521cfc0b0c5b88ed58303cb9417bb672
CP5S36 <yokohama.vcp@gmail.com>
2019-01-11 16:24:59 +0900

    メインシーンを少し変更しました リセットボタンを実装しました

---
+++ AgonyCube/Assets/Scripts/MainStage/Mindata.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mindata {
+
+    public int[] swapMin = { 1, 2, 3, 2, 0, 0, 0, 0, 1, 1};
+    public int[] spinMin = { 0, 0, 0, 0, 1, 2, 1, 1, 1, 1 };
+}
