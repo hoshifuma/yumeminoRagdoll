dc8096aec70693639837cff0f11e664281297ba6
hoshi <fyhcs0605@gmail.com>
2019-01-14 02:11:28 +0900

    bug fixed

--- AgonyCube/Assets/Scripts/Animation/TestAni.cs
+++ AgonyCube/Assets/Scripts/Animation/TestAni.cs
@@ -1,7 +1,8 @@
-﻿using AgonyCubeMainStage;
+﻿
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 namespace AgonyCube.MainStage {
     public class TestAni : MonoBehaviour {
 
