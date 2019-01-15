7bf0b908a1b6b29bf15a5911bfd7deb4c063031f
CP5S33 <yokohama.vcp@gmail.com>
2019-01-15 15:09:48 +0900

    音量の調整

---
+++ AgonyCube/Assets/Scripts/Title/SiblingIndex.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SiblingIndex : MonoBehaviour
+{
+    public GameObject button1;
+    public GameObject button2;
+    public GameObject button3;
+    public GameObject button4;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void siblingIndex1()
+    {
+        button1.transform.SetSiblingIndex(4);
+    }
+
+    public void siblingIndex2()
+    {
+        button2.transform.SetSiblingIndex(4);
+    }
+
+    public void siblingIndex3()
+    {
+        button3.transform.SetSiblingIndex(4);
+    }
+
+    public void siblingIndex4()
+    {
+        button4.transform.SetSiblingIndex(4);
+    }
+}
